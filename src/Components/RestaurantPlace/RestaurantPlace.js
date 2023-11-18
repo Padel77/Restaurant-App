d974dcbc0551149adb781fc9c515b56193187da3
Adel Assem <46504605+Padel77@users.noreply.github.com>
2023-11-18 13:38:07 +0200

    Update RestaurantPlace.js

--- src/Components/RestaurantPlace/RestaurantPlace.js
+++ src/Components/RestaurantPlace/RestaurantPlace.js
@@ -5,7 +5,17 @@ import axios from "axios";
 function RestaurantPlace() {
 // const [restaurant, setRestaurant] = React.useState([]);
 
-
+    axios.get('https://nextjs-orpin-omega-98.vercel.app/api/restaurants')
+        .then(response => {
+            // Handle the successful response
+            console.log('Response:', response.data); // Access the response data
+            // Perform operations with the response data here
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the request
+            console.error('Error fetching data:', error);
+            // Handle the error appropriately, such as showing an error message to the user
+        });
 
 // Make a GET request to the API with the configured headers
 
